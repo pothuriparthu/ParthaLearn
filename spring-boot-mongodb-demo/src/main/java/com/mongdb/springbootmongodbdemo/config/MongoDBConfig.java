aa03b4752c2d0d74269b5b427d86754ecdadf68e
papothur <papothur@PAPOTHUR-HVW0U.partnet.cisco.com>
2019-03-19 05:55:45 +0530

    changes

--- spring-boot-mongodb-demo/src/main/java/com/mongdb/springbootmongodbdemo/config/MongoDBConfig.java
+++ spring-boot-mongodb-demo/src/main/java/com/mongdb/springbootmongodbdemo/config/MongoDBConfig.java
@@ -3,12 +3,9 @@
  */
 package com.mongdb.springbootmongodbdemo.config;
 
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-import com.mongdb.springbootmongodbdemo.model.Users;
 import com.mongdb.springbootmongodbdemo.repository.UsersRepository;
 
 /**
@@ -20,18 +17,17 @@ import com.mongdb.springbootmongodbdemo.repository.UsersRepository;
 @Configuration
 public class MongoDBConfig {
 
-	/*@Bean
-	CommandLineRunner commandLineRunner(UsersRepository usersRepository) {
-		return new CommandLineRunner() {
-
-			@Override
-			public void run(String... args) throws Exception {
-
-				usersRepository.save(new Users(1, "Partha", "Development", 3000L));
-				usersRepository.save(new Users(2, "Arul", "Development", 3500L));
-
-			}
-
-		};
-	}*/
+	/*
+	 * @Bean CommandLineRunner commandLineRunner(UsersRepository usersRepository) {
+	 * return new CommandLineRunner() {
+	 * 
+	 * @Override public void run(String... args) throws Exception {
+	 * 
+	 * usersRepository.save(new Users(1, "Partha", "Development", 3000L));
+	 * usersRepository.save(new Users(2, "Arul", "Development", 3500L));
+	 * 
+	 * }
+	 * 
+	 * }; }
+	 */
 }
